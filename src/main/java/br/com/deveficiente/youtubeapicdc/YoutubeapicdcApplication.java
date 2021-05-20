68f3368d72a3c83ca0c587a3b4aec3217579ca69
Marcell Alves <marcell.alves@gmail.com>
2021-05-20 09:07:30 -0300

    projeto configurado

---
+++ src/main/java/br/com/deveficiente/youtubeapicdc/YoutubeapicdcApplication.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.youtubeapicdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeapicdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeapicdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/deveficiente/youtubeapicdc/YoutubeapicdcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.youtubeapicdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeapicdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
